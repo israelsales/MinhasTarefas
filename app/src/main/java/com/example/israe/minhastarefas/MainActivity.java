0c27d8d55f3381c2bcada7bce9523493f743e4f8
israe <raell.com@gmail.com>
2017-06-26 17:13:13 -0300

    n

--- app/src/main/java/com/example/israe/minhastarefas/MainActivity.java
+++ app/src/main/java/com/example/israe/minhastarefas/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.israe.minhastarefas;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,18 +28,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try{
-            //Acessar ou CriarBanco
-            sqLiteDatabase = openOrCreateDatabase("appminhastarefas",MODE_PRIVATE,null);
+        editTextInserirTarefa = (EditText) findViewById(R.id.editTextTarefaId);
+        buttonAdiconarTarefa = (Button) findViewById(R.id.buttonAdicinarId);
+        listViewListaTarefas = (ListView) findViewById(R.id.listViewTarefasId);
+        recarregarListaTarefas();
+        buttonAdiconarTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Capiturar os dados digitados pelo usuario
+                String stringInserirTarefa = editTextInserirTarefa.getText().toString();
+                if(stringInserirTarefa.equals("")){
+                    Toast.makeText(getApplicationContext(), "Digite uma tarefa", Toast.LENGTH_SHORT).show();
+                }{
+                    try{
+                        sqLiteDatabase.execSQL("INSERT INTO tarefas (tarefa) VALUES ('" + stringInserirTarefa + "')");
+                        recarregarListaTarefas();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
 
-            //Criar tabela
-            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
-
-            editTextInserirTarefa = (EditText)findViewById(R.id.editTextTarefaId);
-            buttonAdiconarTarefa = (Button) findViewById(R.id.buttonAdicinarId);
-            listViewListaTarefas = (ListView)findViewById(R.id.listViewTarefasId);
-
-
+    private void recarregarListaTarefas() {
+        try {
             stringArrayListTarefas = new ArrayList<>();
             stringArrayAdapterTarefas = new ArrayAdapter<String>(
                     getApplicationContext(),
@@ -47,35 +60,25 @@ public class MainActivity extends AppCompatActivity {
                     android.R.id.text1,
                     stringArrayListTarefas
             );
-            listViewListaTarefas.setAdapter(stringArrayAdapterTarefas);
 
+            listViewListaTarefas.setAdapter(stringArrayAdapterTarefas);
+            //Acessar ou CriarBanco
+            sqLiteDatabase = openOrCreateDatabase("appminhastarefas", MODE_PRIVATE, null);
+            //Criar tabela
+            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
             //Recuperar dados banco
-            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM tarefas",null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM tarefas ORDER BY id DESC", null);
             cursor.moveToFirst();
 
-            while (cursor!=null){
+            while (cursor != null) {
                 int indiceColunaTarefas = cursor.getColumnIndex("tarefa");
                 String string = cursor.getString(indiceColunaTarefas);
                 stringArrayListTarefas.add(string);
                 cursor.moveToNext();
-                Log.i("Resultado - ", " Tarefa: " +string);
+                Log.i("Resultado - ", " Tarefa: " + string);
             }
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        buttonAdiconarTarefa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Capiturar os dados digitados pelo usuario
-                String stringInserirTarefa = editTextInserirTarefa.getText().toString();
-                sqLiteDatabase.execSQL("INSERT INTO tarefas (tarefa) VALUES ('"+ stringInserirTarefa +"')");
-
-
-
-
-            }
-        });
     }
 }
