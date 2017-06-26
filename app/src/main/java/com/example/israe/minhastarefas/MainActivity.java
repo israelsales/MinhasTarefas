9be4e58fe09c868de5afdf060a89828b5c10f4a1
israe <raell.com@gmail.com>
2017-06-26 02:02:17 -0300

    n

--- app/src/main/java/com/example/israe/minhastarefas/MainActivity.java
+++ app/src/main/java/com/example/israe/minhastarefas/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.israe.minhastarefas;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -25,25 +28,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Acessar ou CriarBanco
-        SQLiteDatabase.openOrCreateDatabase("appminhastarefas",null);
+        try{
+            //Acessar ou CriarBanco
+            sqLiteDatabase = openOrCreateDatabase("appminhastarefas",MODE_PRIVATE,null);
 
-        //Criar tabela
-        sqLiteDatabase.execSQL("CREATE TABLE IF NO EXIST tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
+            //Criar tabela
+            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
 
-        editTextInserirTarefa = (EditText)findViewById(R.id.editTextTarefaId);
-        buttonAdiconarTarefa = (Button) findViewById(R.id.buttonAdicinarId);
-        listViewListaTarefas = (ListView)findViewById(R.id.listViewTarefasId);
-        stringArrayListTarefas = new ArrayList<>();
+            editTextInserirTarefa = (EditText)findViewById(R.id.editTextTarefaId);
+            buttonAdiconarTarefa = (Button) findViewById(R.id.buttonAdicinarId);
+            listViewListaTarefas = (ListView)findViewById(R.id.listViewTarefasId);
 
-        stringArrayAdapterTarefas = new ArrayAdapter<String>(
-                getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                stringArrayListTarefas
-        );
 
-        listViewListaTarefas.setAdapter(stringArrayAdapterTarefas);
+            stringArrayListTarefas = new ArrayList<>();
+            stringArrayAdapterTarefas = new ArrayAdapter<String>(
+                    getApplicationContext(),
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    stringArrayListTarefas
+            );
+            listViewListaTarefas.setAdapter(stringArrayAdapterTarefas);
+
+            //Recuperar dados banco
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM tarefas",null);
+            cursor.moveToFirst();
+
+            while (cursor!=null){
+                int indiceColunaTarefas = cursor.getColumnIndex("tarefa");
+                String string = cursor.getString(indiceColunaTarefas);
+                stringArrayListTarefas.add(string);
+                cursor.moveToNext();
+                Log.i("Resultado - ", " Tarefa: " +string);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         buttonAdiconarTarefa.setOnClickListener(new View.OnClickListener() {
             @Override
